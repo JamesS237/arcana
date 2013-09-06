f4deb9f0eec63856c8e136561d91ce5edce45a79
Arvoreniad <alexnielsen12@gmail.com>
2013-09-06 18:51:56 +1000

    Switched to rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Arcana::Application.config.secret_key_base = '4c0a7c2dd02fa0527b920073afb3aa0aa39409ef02209bbe2036044009276445688b9d9c219bea7f8341a5b1e2246c0e6b55a2ea0d23652ff450c858f5ccd853'
