97b24c11ef65258f627af97ea38bf3fc8cccadd3
Arvoreniad <alexnielsen12@gmail.com>
2013-12-13 12:38:13 +1100

    added term selection logic to result.rb

--- app/models/result.rb
+++ app/models/result.rb
@@ -5,4 +5,102 @@ class Result < ActiveRecord::Base
   validates :student_id, presence: true
   validates :mark, presence: true
   validates :assessment_id, presence: true
+
+  def set_term
+  	case self.subject.name
+    when "History"
+      case self.student.house_name
+      when "Aherne"
+        self.term = self.assessment.term + 2
+      when "Frew"
+        self.term = self.assessment.term + 2
+      when "Jenkin"
+        self.term =
+      when "Jones"
+        self.term = self.assessment.term + 2
+      when "Millward"
+        self.term = self.assessment.term
+      when "Riley"
+        self.tevrm += 2
+      end
+    when "Geography"
+      case self.student.house_name
+      when "Aherne"
+        self.term = self.assessment.term + 2
+      when "Frew"
+        self.term = self.assessment.term
+      when "Jenkin"
+        self.term = self.assessment.term + 2
+      when "Jones"
+        self.term = self.assessment.term
+      when "Millward"
+        self.term = self.assessment.term + 2
+      when "Riley"
+        self.term = self.assessment.term
+      end
+    when "Music"
+      case self.student.house_name
+      when "Aherne"
+        self.term = self.assessment.term + 2
+      when "Frew"
+        self.term = self.assessment.term + 2
+      when "Jenkin"
+        self.term = self.assessment.term + 2
+      when "Jones"
+        self.term = self.assessment.term
+      when "Millward"
+        self.term = self.assessment.term
+      when "Riley"
+        self.term = self.assessment.term
+      end
+    when "Art"
+      case self.student.house_name
+      when "Aherne"
+        self.term = self.assessment.term 
+      when "Frew"
+        self.term = self.assessment.term
+      when "Jenkin"
+        self.term = self.assessment.term 
+      when "Jones"
+        self.term = self.assessment.term + 2
+      when "Millward"
+        self.term = self.assessment.term + 2
+      when "Riley"
+        self.term = self.assessment.term + 2
+      end
+    when "Religious Education"
+      case self.student.house_name
+      when "Aherne"
+        self.term = self.assessment.term + 2
+      when "Frew"
+        self.term = self.assessment.term + 2
+      when "Jenkin"
+        self.term = self.assessment.term + 2
+      when "Jones"
+        self.term = self.assessment.term
+      when "Millward"
+        self.term = self.assessment.term 
+      when "Riley"
+        self.term = self.assessment.term
+      end
+    when "Personal Development"
+      case self.student.house_name
+      when "Aherne"
+        self.term = self.assessment.term 
+      when "Frew"
+        self.term = self.assessment.term
+      when "Jenkin"
+        self.term = self.assessment.term 
+      when "Jones"
+        self.term = self.assessment.term + 2
+      when "Millward"
+        self.term = self.assessment.term + 2
+      when "Riley"
+        self.term = self.assessment.term + 2
+      end
+    else 
+      self.term = self.assessment.term
+    end
+  end
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131213005451) do
+ActiveRecord::Schema.define(version: 20131213012202) do
 
   create_table "assessments", force: true do |t|
     t.integer  "subject_id"
@@ -42,6 +42,7 @@ ActiveRecord::Schema.define(version: 20131213005451) do
     t.decimal  "mark"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "term"
   end
 
   create_table "students", force: true do |t|
