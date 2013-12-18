4e95f471abfd37fdc100e7c644b4ac8f9e0c488c
Arvoreniad <alexnielsen12@gmail.com>
2013-12-19 10:19:12 +1100

    added mean average function

--- app/models/subject.rb
+++ app/models/subject.rb
@@ -1,5 +1,6 @@
 class Subject < ActiveRecord::Base
   has_many :assessments
+  has_many :averages
   
   validates :name, presence: true
 
@@ -10,6 +11,23 @@ class Subject < ActiveRecord::Base
     end
     leader_id = avgs.sort_by!{|avg| avg["average"] }.last["id"]
     Student.find(leader_id);
+  end
+
+  def mean_average
+    overalls = []
+    puts Average.where(:subject_id => self.id).each.inspect
+    Average.where(:subject_id => self.id).each do |a|
+      puts "AVERAGE: #{a.overall}"
+      overalls << a.overall
+    end
+    total = 0
+    count = 0
+    overalls.each do |val|  
+      next unless val != 0 && val != nil
+      count += 1
+      total += val
+    end
+    return (total / count).round(2)
   end
   
   def to_param
