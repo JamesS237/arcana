9c293dfea693d08794b002842348bff42d2152da
Arvoreniad <alexnielsen12@gmail.com>
2013-12-28 19:44:00 +1100

    search js working

--- app/assets/javascripts/static_pages.js
+++ app/assets/javascripts/static_pages.js
@@ -4,24 +4,24 @@ $(document).ready(function () {
 			$('#search-main').focus();
 		}
 	});
-	if ($('div[data-type="assessments"]').children().length > $('div[data-type="subjects"]').children().length 
-	 && $('div[data-type="assessments"]').children().length > $('div[data-type="students"]').children().length) {
+	if ($('ul[data-type="assessments"]').children().length > $('ul[data-type="subjects"]').children().length 
+	 && $('ul[data-type="assessments"]').children().length > $('ul[data-type="students"]').children().length) {
 		var longest = 'assessments'; 
-	} else if ($('div[data-type="subjects"]').children().length > $('div[data-type="assessments"]').children().length && 
-			   $('div[data-type="subjects"]').children().length > $('div[data-type="students"]').children().length) {
+	} else if ($('ul[data-type="subjects"]').children().length > $('ul[data-type="assessments"]').children().length && 
+			   $('ul[data-type="subjects"]').children().length > $('ul[data-type="students"]').children().length) {
 		var longest = 'subjects'; //if guides is the longest search result, set it
 	} else {
 		var longest = 'students'; //if messages is the longest search result, set it
 	}
 
-	$('div[data-type]').hide(); //hide all search result types
-	$('div[data-type=' + longest + ']').show(); //show the type that has the most results
+	$('ul[data-type]').hide(); //hide all search result types
+	$('ul[data-type=' + longest + ']').show(); //show the type that has the most results
 	$("a[data-view-type=" + longest + "]").addClass('link-highlight'); //add the active color to that type's link
 
 	$("a[data-view-type]").on('click', function() { //when a type link is clicked
-	$('div[data-type]').hide(); //hide all search result types
-	$("a[data-view-type]").removeClass('link-highlight'); //remove the active color from all type links
-	$('div[data-type=' + $(this).data('view-type') + ']').show(); //display the correct search result type
-	$(this).addClass('link-highlight'); //add the active color to that the clicked link
+		$('ul[data-type]').hide(); //hide all search result types
+		$("a[data-view-type]").removeClass('link-highlight'); //remove the active color from all type links
+		$('ul[data-type=' + $(this).data('view-type') + ']').show(); //display the correct search result type
+		$(this).addClass('link-highlight'); //add the active color to that the clicked link
 	});
 });
