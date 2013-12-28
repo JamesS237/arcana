3bd89b41a5272c162371fdc72871973dbb007cd7
Arvoreniad <alexnielsen12@gmail.com>
2013-12-28 18:12:30 +1100

    general routes cleanup

--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,20 @@
 Arcana::Application.routes.draw do
-  get "static_pages/home"
-  get "static_pages/about"
+  root 'static_pages#home'
+
   match '/results', to: 'results#update', via: 'put'
   match '/results', to: 'results#create', via: 'post'
-  match '/signup',     to: 'students#new',            via: 'get'
-  match '/about',     to: 'static_pages#about',            via: 'get'
-  match '/legal',     to: 'static_pages#legal',            via: 'get'
-  match '/signin',    to: 'sessions#new',         via: 'get'
-  match '/signout',    to: 'sessions#destroy',         via: 'get'
   match '/:name/results', to: 'results#user', via: 'get', as: :student_results
   match '/results/:id', to: 'results#update', via: 'post'
+
+  match '/about',     to: 'static_pages#about',            via: 'get'
+  match '/legal',     to: 'static_pages#legal',            via: 'get'
+  
+  match '/signup',     to: 'students#new',            via: 'get'
+  match '/signin',    to: 'sessions#new',         via: 'get'
+  match '/signout',    to: 'sessions#destroy',         via: 'get'
+
   match '/assessments/subject/:name', to: 'assessments#index', via: 'get'
   match '/search', to: 'static_pages#search', via: 'get'
-
 
   resources :students
   resources :assessments
@@ -20,6 +22,4 @@ Arcana::Application.routes.draw do
   resources :subjects
   resources :types
   resources :sessions, only: [:new, :create, :destroy]
-  
-  root 'static_pages#home'
 end
