fecbe34b1bc15dbc5eb7b0f0f1b70323ff859d7d
Arvoreniad <alexnielsen12@gmail.com>
2014-01-23 09:53:16 +1100

    added class infos

--- app/models/result.rb
+++ app/models/result.rb
@@ -1,7 +1,7 @@
 class Result < ActiveRecord::Base
   belongs_to :assessment
   belongs_to :student
-  
+
   validates :student_id, presence: true
   validates :mark, presence: true
   validates :assessment_id, presence: true
@@ -9,97 +9,68 @@ class Result < ActiveRecord::Base
   def set_term
   	case self.assessment.subject.name
     when "History"
-      #c m d v w h
       case self.student.class_group
       when "c"
         self.term = self.assessment.term + 2
       when "m"
-        self.term = self.assessment.term + 2
+        self.term = self.assessment.term
       when "d"
-        self.term = self.assessment.term
+        self.term = self.assessment.term + 2
       when "v"
-        self.term = self.assessment.term + 2
-      when "w"
         self.term = self.assessment.term
-      when "h"
+      when "w"
         self.term = self.assessment.term + 2
+      when "h"
+        self.term = self.assessment.term
       end
     when "Geography"
       case self.student.class_group
       when "c"
-        self.term = self.assessment.term + 2
+        self.term = self.assessment.term
       when "m"
-        self.term = self.assessment.term
-      when "v"
-        self.term = self.assessment.term + 2
-      when "w"
-        self.term = self.assessment.term
-      when "d"
-        self.term = self.assessment.term + 2
-      when "h"
-        self.term = self.assessment.term
-      end
-    when "Music"
-      case self.student.class_group
-      when "h"
         self.term = self.assessment.term + 2
       when "v"
-        self.term = self.assessment.term + 2
+        self.term = self.assessment.term
       when "w"
         self.term = self.assessment.term + 2
       when "d"
         self.term = self.assessment.term
-      when "m"
-        self.term = self.assessment.term
-      when "c"
-        self.term = self.assessment.term
-      end
-    when "Art"
-      case self.student.class_group
       when "h"
-        self.term = self.assessment.term 
-      when "v"
-        self.term = self.assessment.term
-      when "w"
-        self.term = self.assessment.term 
-      when "d"
-        self.term = self.assessment.term + 2
-      when "m"
-        self.term = self.assessment.term + 2
-      when "c"
         self.term = self.assessment.term + 2
       end
+    #RE: v h c
+    #Drama: w m d
     when "Religious Education"
       case self.student.class_group
       when "h"
-        self.term = self.assessment.term + 2
+        self.term = self.assessment.term
       when "v"
-        self.term = self.assessment.term + 2
+        self.term = self.assessment.term
       when "w"
         self.term = self.assessment.term + 2
       when "d"
-        self.term = self.assessment.term
+        self.term = self.assessment.term + 2
       when "m"
-        self.term = self.assessment.term 
+        self.term = self.assessment.term + 2
       when "c"
         self.term = self.assessment.term
       end
     when "Personal Development"
       case self.student.class_group
       when "h"
-        self.term = self.assessment.term 
+        self.term = self.assessment.term + 2
       when "v"
-        self.term = self.assessment.term
+        self.term = self.assessment.term + 2
       when "w"
-        self.term = self.assessment.term 
+        self.term = self.assessment.term
       when "d"
-        self.term = self.assessment.term + 2
+        self.term = self.assessment.term
       when "m"
-        self.term = self.assessment.term + 2
+        self.term = self.assessment.term
       when "c"
         self.term = self.assessment.term + 2
       end
-    else 
+    else
       self.term = self.assessment.term
     end
   end
