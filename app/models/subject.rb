ab2acde43592f07ee7cc7479705b60acab751a84
Arvoreniad <alexnielsen12@gmail.com>
2014-01-30 20:23:02 +1100

    updated get leader code for redis

--- app/models/subject.rb
+++ app/models/subject.rb
@@ -1,18 +1,11 @@
 class Subject < ActiveRecord::Base
   has_many :assessments
   has_many :averages
-  
+
   validates :name, presence: true
 
-  def leader 
-    avgs = []
-    Average.where(:subject_id => self.id).each do |avg| 
-      avgs.push({"id" => avg.student_id, "average" => avg.overall})
-    end
-    if avgs == []
-      return nil 
-    end
-    leader_id = avgs.sort_by!{|avg| avg["average"] }.last["id"]
+  def leader
+    leader_id = $redis.zrevrange("results:#{self.redis_name}", 0, 0)[1].to_i
     Student.find(leader_id);
   end
 
@@ -27,14 +20,14 @@ class Subject < ActiveRecord::Base
     end
     total = 0
     count = 0
-    overalls.each do |val|  
+    overalls.each do |val|
       next unless val != 0 && val != nil
       count += 1
       total += val
     end
     return (total / count).round(2)
   end
-  
+
   def to_param
     self.name.gsub(' ', '-')
   end
