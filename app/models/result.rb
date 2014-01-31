f6fc45a8f6041a23779f47ae38d94eb1fbe7fdea
Arvoreniad <alexnielsen12@gmail.com>
2014-01-31 17:37:14 +1100

    built new set term function

--- app/models/result.rb
+++ app/models/result.rb
@@ -17,72 +17,7 @@ class Result < ActiveRecord::Base
   }
 
   def set_term!
-  	case self.assessment.subject.name
-    when "History"
-      case self.student.class_group
-      when "c"
-        self.term = self.assessment.term + 2
-      when "m"
-        self.term = self.assessment.term
-      when "d"
-        self.term = self.assessment.term + 2
-      when "v"
-        self.term = self.assessment.term
-      when "w"
-        self.term = self.assessment.term + 2
-      when "h"
-        self.term = self.assessment.term
-      end
-    when "Geography"
-      case self.student.class_group
-      when "c"
-        self.term = self.assessment.term
-      when "m"
-        self.term = self.assessment.term + 2
-      when "v"
-        self.term = self.assessment.term
-      when "w"
-        self.term = self.assessment.term + 2
-      when "d"
-        self.term = self.assessment.term
-      when "h"
-        self.term = self.assessment.term + 2
-      end
-    #RE: v h c
-    #Drama: w m d
-    when "Religious Education"
-      case self.student.class_group
-      when "h"
-        self.term = self.assessment.term
-      when "v"
-        self.term = self.assessment.term
-      when "w"
-        self.term = self.assessment.term + 2
-      when "d"
-        self.term = self.assessment.term + 2
-      when "m"
-        self.term = self.assessment.term + 2
-      when "c"
-        self.term = self.assessment.term
-      end
-    when "Personal Development"
-      case self.student.class_group
-      when "h"
-        self.term = self.assessment.term + 2
-      when "v"
-        self.term = self.assessment.term + 2
-      when "w"
-        self.term = self.assessment.term
-      when "d"
-        self.term = self.assessment.term
-      when "m"
-        self.term = self.assessment.term
-      when "c"
-        self.term = self.assessment.term + 2
-      end
-    else
-      self.term = self.assessment.term
-    end
+    self.term = self.assessment.term + @term_data[self.assessment.subject.name][self.student.class_group]
   end
 
   def update_averages!
