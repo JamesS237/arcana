6f41f0d291af5079879365e1efab4b1ce6da84ac
Arvoreniad <alexnielsen12@gmail.com>
2014-02-01 15:53:25 +1100

    added redis config for appfog

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,1 +1,11 @@
-$redis = Redis.new(:host => 'localhost', :port => 6379)
+if(Rails.env.development?)
+  $redis = Redis.new(:host => 'localhost', :port => 6379)
+else
+  configure do
+      services = JSON.parse(ENV['VCAP_SERVICES'])
+      redis_key = services.keys.select { |svc| svc =~ /redis/i }.first
+      redis = services[redis_key].first['credentials']
+      redis_conf = {:host => redis['hostname'], :port => redis['port'], :password => redis['password']}
+      $redis = Redis.new redis_conf
+  end
+end
