503647c209318d0582a37c55be2b18cf5d63ee70
Arvoreniad <alexnielsen12@gmail.com>
2014-02-02 15:44:28 +1100

    cut redis init code back to just rediscloud

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,11 +1,5 @@
-if ENV["REDISTOGO_URL"]
-  uri = URI.parse(ENV["REDISTOGO_URL"])
-  $redis = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
-elsif ENV["REDISCLOUD_URL"]
+if ENV["REDISCLOUD_URL"]
   uri = URI.parse(ENV["REDISCLOUD_URL"])
-  $redis = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
-elsif ENV["OPENREDIS_URL"]
-  uri = URI.parse(ENV["OPENREDIS_URL"])
   $redis = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
 else
   $redis = Redis.new(:host => 'localhost', :port => 6379)
