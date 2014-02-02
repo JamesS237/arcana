97954cba0c4c322c5519b977565d07d4fe72093b
Arvoreniad <alexnielsen12@gmail.com>
2014-02-02 11:59:01 +1100

    another fix for heroku

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,11 +1,1 @@
-if(Rails.env.development?)
-  $redis = Redis.new(:host => 'localhost', :port => 6379)
-else
-  configure do
-      services = JSON.parse(ENV['VCAP_SERVICES'])
-      redis_key = services.keys.select { |svc| svc =~ /redis/i }.first
-      redis = services[redis_key].first['credentials']
-      redis_conf = {:host => redis['hostname'], :port => redis['port'], :password => redis['password']}
-      $redis = Redis.new redis_conf
-  end
-end
+$redis = Redis.new(:host => 'localhost', :port => 6379)
