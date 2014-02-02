7ad1a133c5fc12a2ba8d5efe4aff299d1c2eb494
Arvoreniad <alexnielsen12@gmail.com>
2014-02-02 11:09:22 +1100

    initial bower config

--- config/application.rb
+++ config/application.rb
@@ -8,9 +8,12 @@ Bundler.require(:default, Rails.env)
 
 module Arcana
   class Application < Rails::Application
-    config.action_view.field_error_proc = Proc.new { |html_tag, instance| 
-    i"#{html_tag}".html_safe 
+    config.action_view.field_error_proc = Proc.new { |html_tag, instance|
+    i"#{html_tag}".html_safe
     }
+
+    config.assets.paths << Rails.root.join('vendor', 'assets', 'components')
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
